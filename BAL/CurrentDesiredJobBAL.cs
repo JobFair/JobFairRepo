8bebfdee523c520dbd8feab933174dc7859f2876
jyoti.logossolutions <jyoti.logossolutions@gmail.com>
2015-09-07 18:37:37 +0530

    professional details user control, user details form with tab, modification in register stored procedure

---
+++ BAL/CurrentDesiredJobBAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BAL
+{
+    public class CurrentDesiredJobBAL
+    {
+        public int CurrentProfessionalDetailsBAL(Entities.JobSeeker.CurrentDesiredJobEntity curentity)
+        {
+            CurrentDesiredJobDAL cdjDAL=new CurrentDesiredJobDAL();
+           return cdjDAL.CurrentProfessionalDetailsDAL(curentity);
+           
+        }
+    }
+}
