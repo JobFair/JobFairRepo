bfdf169e41ef4d417f7f3cee8244e526e1e4c6fa
saurabh.logossolutions <saurabh.logossolutions@gmail.com>
2015-09-11 11:26:49 +0530

    Affirmative conflicts resolved

---
+++ DAL/CurrentDesiredJobDAL.cs
@@ -0,0 +1,73 @@
+﻿using Entities.JobSeeker;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class CurrentDesiredJobDAL
+    {
+         private SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["JobPortalCon"].ToString());
+
+
+    
+         public int CurrentProfessionalDetailsDAL(CurrentDesiredJobEntity curentity)
+         {
+                 try
+                 {
+                     connection.Open();
+                      SqlCommand cmd = new SqlCommand();
+
+                      SqlParameter[] sparams = { new SqlParameter("@candidateId",curentity.Candidateid),
+                                            new SqlParameter("@resumeheadline",curentity.ResumeHeadline),
+                                            new SqlParameter("@totalExp",curentity.TotalExperience),
+                                            new SqlParameter("@industryId",curentity.Industry),
+                                            new SqlParameter("@departmentId",curentity.Department),
+                                            new SqlParameter("@currentjobrole",curentity.CurrentJobRole),
+                                            new SqlParameter("@currentemployer",curentity.CurrentEmployer),
+                                            new SqlParameter("@primfunrole",curentity.PrimFunctionalRole),
+                                            new SqlParameter("@primjobdesrip",curentity.PrimJobDescrip),
+                                            new SqlParameter("@primtechskills",curentity.PrimTechSkills),
+                                            new SqlParameter("@secfunrole",curentity.SecFunctionalRole),
+                                            new SqlParameter("@secjobdescrip",curentity.SecJobDescrip),
+                                            new SqlParameter("@sectechskills",curentity.SecTechSkills),
+                                            new SqlParameter("@technicalskills",curentity.TechnicalSkills),
+                                            new SqlParameter("@reasonforjobchange",curentity.ReasonforJobChange),
+                                            new SqlParameter("@designation",curentity.Designation)};
+
+                      int res = SqlHelper.ExecuteNonQuery(connection, CommandType.StoredProcedure, "sp_JS_InsertProfessionalDetails", sparams);
+                     return res;
+                 }
+
+                 catch (Exception ex)
+                 {
+                     throw ex;
+                 }
+                 finally
+                 {
+                     connection.Close();
+                 }
+         }
+
+         public DataTable GetDepartmentDAL()
+         {
+             connection.Open();
+             SqlCommand cmd = new SqlCommand("select * from Departments", connection);
+             SqlDataAdapter da = new SqlDataAdapter(cmd);
+             DataTable dt = new DataTable();
+             da.Fill(dt);
+             return dt;
+         }
+
+         public DataTable GetIndustryDAL()
+         {
+             connection.Open();
+             SqlCommand cmd=new SqlCommand("select * from Industry",connection);
+             SqlDataAdapter da=new SqlDataAdapter(cmd);
+             DataTable dt = new DataTable();
+             da.Fill(dt);
+             return dt;
+         }
+    }
+    }
--- JobFair/Forms/JobSeeker/ProfessionalDetailsJobSeeker.aspx.cs
+++ JobFair/Forms/JobSeeker/ProfessionalDetailsJobSeeker.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Entities;
+using Entities.JobSeeker;
 using BAL;
 using CommonUtil;
 
@@ -23,20 +23,20 @@ namespace JobFair.Forms.JobSeeker
         private void BindDropDownDepartment()
         {
             //ddlDepartment.DataSource = Utility.GetDepartmentBAL();
-            ddlDepartment.DataTextField = "DepartmentName";
-            ddlDepartment.DataValueField = "DeptID";
-            ddlDepartment.DataBind();
-            ddlDepartment.Items.Insert(0, new ListItem("--Select--", "0"));
+            //ddlDepartment.DataTextField = "DepartmentName";
+            //ddlDepartment.DataValueField = "DepartmentId";
+            //ddlDepartment.DataBind();
+            //ddlDepartment.Items.Insert(0, new ListItem("--Select--", "0"));
         }
 
         private void BindDropDownIndustry()
         {
 
-           // ddlIndustry.DataSource= Utility.GetIndustryBAL();
-            ddlIndustry.DataTextField = "Industry_Name";
-            ddlIndustry.DataValueField = "Ind_ID";
-            ddlIndustry.DataBind();
-            ddlIndustry.Items.Insert(0, new ListItem("--Select--", "0"));
+            //ddlIndustry.DataSource= Utility.GetIndustryBAL();
+            //ddlIndustry.DataTextField = "IndustryName";
+            //ddlIndustry.DataValueField = "IndustryId";
+            //ddlIndustry.DataBind();
+            //ddlIndustry.Items.Insert(0, new ListItem("--Select--", "0"));
 
            
         }
@@ -47,51 +47,52 @@ namespace JobFair.Forms.JobSeeker
         /// <param name="e">The <cref="EventArgs">instance containing event data</param>
         protected void btnSave_Click(object sender, EventArgs e)
         {
-            try
-            {
-                //calculting total experience     
-                DateTime FromYear = Convert.ToDateTime(txtFromdate.Text);
-                DateTime ToYear = Convert.ToDateTime(txtTill.Text);
-                //Creating object of TimeSpan Class  
-                TimeSpan objTimeSpan = ToYear - FromYear;
-                //years  
-                int Years = ToYear.Year - FromYear.Year;
-                int Month = ToYear.Month - FromYear.Month;
-                Label1.Text = Years + "Years-" + Month + "Months";
-                ProfessionalDetailsEntity curentity = new ProfessionalDetailsEntity();
-                //ProfessionalDetailsCurrentJSBAL rjsBAL = new ProfessionalDetailsCurrentJSBAL();
-                curentity.Userid = "1200132426";
-                curentity.ResumeHeadline = txtResumeHeadline.Text;
-                curentity.TotalExperience = (Years + '.' + Month);
-                curentity.Industry = ddlIndustry.SelectedItem.Text;
-                curentity.Department = ddlDepartment.SelectedItem.Text;
-                curentity.CurrentJobRole = txtCurrentJobRole.Text;
-                curentity.PrimFunctionalRole = ddlPrimaryRole.SelectedItem.Text;
-                curentity.PrimJobDescrip = txtJobdescriptionPrimar.Text;
-                curentity.PrimTechSkills = txtTechnicalskillPrimary.Text;
-                curentity.SecFunctionalRole = ddlSecRole.SelectedItem.Text;
-                curentity.SecJobDescrip = txtjobdescriptionSec.Text;
-                curentity.SecTechSkills = txtTechnicalskillSec.Text;
-                curentity.Designation = txtDesignation.Text;
-                curentity.ReasonforJobChange = txtReasonforJobchange.Text;
-                curentity.CurrentEmployer = txtemployeer.Text;
-                curentity.TechnicalSkills = txtTechSkills.Text;
-                //int result = rjsBAL.CurrentProfessionalDetailsBAL(curentity);
-                //if (result > 0)
-                //{
-                //    lblmsg.Text = "Your details saved successfully";
-                //}
-                //else
-                //{
-                //    lblmsg.Text = "Your details are not saved";
-                //}
+            //try
+            //{
+            //    //calculting total experience     
+            //    DateTime FromYear = Convert.ToDateTime(txtFromdate.Text);
+            //    DateTime ToYear = Convert.ToDateTime(txtTill.Text);
+            //    //Creating object of TimeSpan Class  
+            //    TimeSpan objTimeSpan = ToYear - FromYear;
+            //    //years  
+            //    int Years = ToYear.Year - FromYear.Year;
+            //    int Month = ToYear.Month - FromYear.Month;
+            //    Label1.Text = Years + "Years-" + Month + "Months";
+            //    CurrentDesiredJobEntity curentity = new CurrentDesiredJobEntity();
+            //    CurrentDesiredJobBAL curjobBAL = new CurrentDesiredJobBAL();
+            //    //ProfessionalDetailsCurrentJSBAL rjsBAL = new ProfessionalDetailsCurrentJSBAL();
+            //    curentity.Candidateid = "JS9";
+            //    curentity.ResumeHeadline = txtResumeHeadline.Text;
+            //    curentity.TotalExperience = "oio" ;
+            //    curentity.Industry = ddlIndustry.SelectedIndex;
+            //    curentity.Department = ddlDepartment.SelectedIndex;
+            //    curentity.CurrentJobRole = txtCurrentJobRole.Text;
+            //    curentity.PrimFunctionalRole = ddlPrimaryRole.SelectedItem.Text;
+            //    curentity.PrimJobDescrip = txtJobdescriptionPrimar.Text;
+            //    curentity.PrimTechSkills = txtTechnicalskillPrimary.Text;
+            //    curentity.SecFunctionalRole = ddlSecRole.SelectedItem.Text;
+            //    curentity.SecJobDescrip = txtjobdescriptionSec.Text;
+            //    curentity.SecTechSkills = txtTechnicalskillSec.Text;
+            //    curentity.Designation = txtDesignation.Text;
+            //    curentity.ReasonforJobChange = txtReasonforJobchange.Text;
+            //    curentity.CurrentEmployer = txtemployeer.Text;
+            //    curentity.TechnicalSkills = txtTechSkills.Text;
+            //    int result = curjobBAL.CurrentProfessionalDetailsBAL(curentity);
+            //    if (result > 0)
+            //    {
+            //        lblmsg.Text = "Your details saved successfully";
+            //    }
+            //    else
+            //    {
+            //        lblmsg.Text = "Your details are not saved";
+            //    }
 
-            }
-            catch (Exception ex)
-            {
+            //}
+            //catch (Exception ex)
+            //{
 
-                throw ex;
-            }
+            //    throw ex;
+            //}
 
         }
 
