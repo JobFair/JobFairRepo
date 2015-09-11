5315f42c0a5b2d905c35adf6205bd69a704ef20b
jyoti.logossolutions <jyoti.logossolutions@gmail.com>
2015-09-11 15:29:19 +0530

    Professionaldetails user control, user details pages added

---
+++ BAL/CurrentDesiredJobBAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.JobSeeker;
+using DAL;
+
+namespace BAL
+{
+    public class CurrentDesiredJobBAL
+    {
+        public int CurrentProfessionalDetailsBAL(CurrentDesiredJobEntity curentity)
+        {
+            CurrentDesiredJobDAL cdjDAL = new CurrentDesiredJobDAL();
+            return cdjDAL.CurrentProfessionalDetailsDAL(curentity);
+            
+        }
+    }
+}
