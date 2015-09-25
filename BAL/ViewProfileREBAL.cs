30068a00ae1ef1b113151a0c1fee54438f5dc3d0
saurabh.logossolutions <saurabh.logossolutions@gmail.com>
2015-09-25 18:50:34 +0530

    ViewProfile of DAL,BAL Entity created

---
+++ BAL/ViewProfileREBAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using System.Data;
+using Entities.Recruiter;
+
+namespace BAL
+{
+   public class ViewProfileREBAL
+    {
+       public DataSet ViewProfile(ViewProfileEntity viewProfileEntity)
+       {
+           ViewProfileREDAL viewProfileDAL = new ViewProfileREDAL();
+           return viewProfileDAL.ViewProfileDAL(viewProfileEntity);
+       }
+    }
+}
