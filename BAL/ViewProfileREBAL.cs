1a26f090608072db082ec3d2f1957310ee322d03
saurabh.logossolutions <saurabh.logossolutions@gmail.com>
2015-09-28 14:24:25 +0530

    changes in viewprofile and personalDetails

--- BAL/ViewProfileREBAL.cs
+++ BAL/ViewProfileREBAL.cs
@@ -1,20 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DAL;
-using System.Data;
+﻿using DAL;
 using Entities.Recruiter;
+using System.Data;
 
 namespace BAL
 {
-   public class ViewProfileREBAL
+    public class ViewProfileREBAL
     {
-       public DataSet ViewProfile(ViewProfileEntity viewProfileEntity)
-       {
-           ViewProfileREDAL viewProfileDAL = new ViewProfileREDAL();
-           return viewProfileDAL.ViewProfileDAL(viewProfileEntity);
-       }
+        private ViewProfileREDAL viewProfileDAL = new ViewProfileREDAL();
+
+        /// <summary>
+        /// ViewProfile of recuiter
+        /// </summary>
+        /// <param name="viewProfileEntity">The ViewProfile Entity</param>
+        /// <returns></returns>
+        public DataSet ViewProfile(ViewProfileEntity viewProfileEntity)
+        {
+            return viewProfileDAL.ViewProfileDAL(viewProfileEntity);
+        }
+
+        /// <summary>
+        /// For display posted job of recruiter
+        /// </summary>
+        /// <returns></returns>
+        public DataSet ViewJobPostBAL()
+        {
+            return viewProfileDAL.ViewJobPostDAL();
+        }
     }
 }
--- JobFair/Forms/Recruiter/ViewProfile.aspx.cs
+++ JobFair/Forms/Recruiter/ViewProfile.aspx.cs
@@ -7,20 +7,27 @@ namespace JobFair.Forms.Recruiter
 {
     public partial class ViewProfile : System.Web.UI.Page
     {
+        /// <summary>
+        /// Class ViewProfile
+        /// </summary>
+
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            DataSet ds = new DataSet();
             ViewProfileREBAL viewProfileBAL = new ViewProfileREBAL();
             ViewProfileEntity viewProfileEntity = new ViewProfileEntity();
-           // viewProfileEntity.name = ds.Tables["RecruiterfullName"].ToString();
             viewProfileBAL.ViewProfile(viewProfileEntity);
-            Label3.Text = viewProfileEntity.name;
-            Label4.Text = viewProfileEntity.companyName;
-            Label5.Text = viewProfileEntity.mobNo;
-            Label6.Text = viewProfileEntity.emailId;
-            Label7.Text = viewProfileEntity.city;
+            //Set the value of viewProfileEntity
+            lblRecruiterName.Text = viewProfileEntity.name;
+            lblCompanyName.Text = viewProfileEntity.companyName;
+            lblMobile.Text = viewProfileEntity.mobNo;
+            lblEmail.Text = viewProfileEntity.emailId;
+            lblCity.Text = viewProfileEntity.city;
             Image1.ImageUrl = viewProfileEntity.photo;
-           
+            //Bind repeater control
+            ds = viewProfileBAL.ViewJobPostBAL();
+            Repeater1.DataSource = ds;
+            Repeater1.DataBind();
         }
     }
 }
