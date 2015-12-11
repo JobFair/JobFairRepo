6ae3fe10601f1126b72d18874ef281ab571b1ed5
jyoti.logossolutions <jyoti.logossolutions@gmail.com>
2015-12-11 18:37:37 +0530

    Added html template

--- DAL/RegisterJobSeekerDAL.cs
+++ DAL/RegisterJobSeekerDAL.cs
@@ -34,12 +34,12 @@ namespace DAL
                 sparams[2] = new SqlParameter("@emailId", registerEntity.EmailId);
                 sparams[3] = new SqlParameter("@gender", registerEntity.Gender);
                 sparams[4] = new SqlParameter("@mobNo", registerEntity.MobileNo);
-                sparams[5] = new SqlParameter("@address", registerEntity.CityId);
-                sparams[6] = new SqlParameter("@countryId", registerEntity.CurrentAddress);
-                sparams[7] = new SqlParameter("@stateId", registerEntity.CurrentAddress);
-                sparams[8] = new SqlParameter("@cityId", registerEntity.CurrentAddress);
-                sparams[9] = new SqlParameter("@cityArea", registerEntity.CurrentAddress);
-                sparams[10] = new SqlParameter("@pincode", registerEntity.CurrentAddress);
+                sparams[5] = new SqlParameter("@address", registerEntity.CurrentAddress);
+                sparams[6] = new SqlParameter("@countryId", registerEntity.Country);
+                sparams[7] = new SqlParameter("@stateId", registerEntity.State);
+                sparams[8] = new SqlParameter("@cityId", registerEntity.CityId);
+                sparams[9] = new SqlParameter("@cityArea", registerEntity.CityArea);
+                sparams[10] = new SqlParameter("@pincode", registerEntity.Pincode);
                 sparams[11] = new SqlParameter("@refCandidatelId", registerEntity.RefCandidateId);
                 sparams[12] = new SqlParameter("@password", registerEntity.Password);
                 sparams[13] = new SqlParameter("@uploadresumepath", registerEntity.UploadResumepath.ToString());
@@ -48,21 +48,21 @@ namespace DAL
 
                 SqlHelper.ExecuteNonQuery(connection, CommandType.StoredProcedure, Constants.sp_JS_InsertRegisterUser, sparams);
 
-                candidateId = Convert.ToString(sparams[10].Value);
+                candidateId = Convert.ToString(sparams[14].Value);
                 if (string.IsNullOrEmpty(candidateId))
                 {
                     return null;
                 }
+                return candidateId;
             }
             catch (Exception)
             {
-                // throw;
+                throw;
             }
             finally
             {
                 connection.Close();
-            }
-            return candidateId;
+            }            
         }
     }
 }
