fb45a45789c9f14d25bcefc81088fb5880ce8bcb
harshal.losgossolutions <harshal.losgossolutions@gmail.com>
2015-12-15 19:12:02 +0530

    commit code

--- JobFair/Forms/JobSeeker/LogIn.aspx.cs
+++ JobFair/Forms/JobSeeker/LogIn.aspx.cs
@@ -32,7 +32,7 @@ namespace JobFair.Forms.JobSeeker
                 }
                 Session["Candidateid"] = candidateID;
 
-                Response.Redirect("WebForm1.aspx");
+                Response.Redirect("AdvanceSearch.aspx");
                 //Response.Redirect("~/UserControls/JobSeeker/DemoEducationalDetails.aspx");
             }
             catch (Exception ex)
