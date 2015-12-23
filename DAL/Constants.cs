0ed960b4f0d9cc308f6237eb9050013934b33005
harshal.losgossolutions <harshal.losgossolutions@gmail.com>
2015-12-23 19:29:45 +0530

    commit code

--- DAL/Constants.cs
+++ DAL/Constants.cs
@@ -91,6 +91,7 @@
         public static string sp_JS_SelectCertification = "sp_JS_SelectCertification";
         public static string sp_JS_InsertRoleSkill = "sp_JS_InsertRoleSkill";
         public static string sp_JS_InsertTechnicalSkills = "sp_JS_InsertTechnicalSkills";
+        public static string sp_InsertJobHistory = "sp_InsertJobHistory";
 
 
     }
