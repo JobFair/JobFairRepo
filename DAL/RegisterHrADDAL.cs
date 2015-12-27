39c99d3f90c60011a88f67b4479afdd1674dd68a
suyoglogossolutions <suyog.logossolutions@gmail.com>
2015-12-27 19:01:14 +0530

    Register Admin

--- DAL/RegisterHrADDAL.cs
+++ DAL/RegisterHrADDAL.cs
@@ -19,37 +19,38 @@ namespace DAL
                 connection.Open();
 
                 SqlCommand cmd = new SqlCommand();
-                SqlParameter[] param = new SqlParameter[26];
-                param[0] = new SqlParameter("@recrName", registerEntity.FullName);
-                param[1] = new SqlParameter("@company", registerEntity.Company);
-                param[2] = new SqlParameter("@officialContactNo", registerEntity.OfficialContactNo);
-                param[3] = new SqlParameter("@mobNo", registerEntity.MobileNo);
-                param[4] = new SqlParameter("@alternateNo", registerEntity.AlternateNo);
-                param[5] = new SqlParameter("@officialEmailid", registerEntity.OfficialEmailId);
-                param[6] = new SqlParameter("@personalMailId", registerEntity.PersonalMailId);
-                param[7] = new SqlParameter("@freelancerOrEmployee", registerEntity.FreelanceOrEmployee);
-                param[8] = new SqlParameter("@amountPerMonth", registerEntity.AmountPerMonth);
-                param[9] = new SqlParameter("@percentPerClosure", registerEntity.PercentPerClosure);
-                param[10] = new SqlParameter("@employeeSalary", registerEntity.EmployeeSalary);
-                param[11] = new SqlParameter("@address", registerEntity.Address);
-                param[12] = new SqlParameter("@presentCountry", registerEntity.PresentCountry);
-                param[13] = new SqlParameter("@presentState", registerEntity.PresentState);
-                param[14] = new SqlParameter("@presentCity", registerEntity.PresentCity);
-                param[15] = new SqlParameter("@presentArea", registerEntity.PresentArea);
-                param[16] = new SqlParameter("@pinCode", registerEntity.Pincode);
-                param[17] = new SqlParameter("@employmentStatus", registerEntity.EmploymentStatus);
-                param[18] = new SqlParameter("@jobType", registerEntity.JobType);
-                param[19] = new SqlParameter("@clientSite", registerEntity.ClientSite);
-                param[20] = new SqlParameter("@clientName", registerEntity.ClientName);
-                param[21] = new SqlParameter("@panCardNo", registerEntity.PANCardNo);
-                param[22] = new SqlParameter("@photoPath", registerEntity.PhotoPath);
-                param[23] = new SqlParameter("@password", registerEntity.Password);
-                param[24] = new SqlParameter("@isMailSent", registerEntity.IsMailSent);
-                param[25] = new SqlParameter("@setHrid", SqlDbType.VarChar, 500);
-                param[25].Direction = ParameterDirection.Output;
+                SqlParameter[] param = new SqlParameter[27];
+                param[0] = new SqlParameter("@adminId", registerEntity.AdminId);
+                param[1] = new SqlParameter("@HrName", registerEntity.FullName);
+                param[2] = new SqlParameter("@company", registerEntity.Company);
+                param[3] = new SqlParameter("@officialContactNo", registerEntity.OfficialContactNo);
+                param[4] = new SqlParameter("@mobNo", registerEntity.MobileNo);
+                param[5] = new SqlParameter("@alternateNo", registerEntity.AlternateNo);
+                param[6] = new SqlParameter("@officialEmailid", registerEntity.OfficialEmailId);
+                param[7] = new SqlParameter("@personalMailId", registerEntity.PersonalMailId);
+                param[8] = new SqlParameter("@freelancerOrEmployee", registerEntity.FreelanceOrEmployee);
+                param[9] = new SqlParameter("@amountPerMonth", registerEntity.AmountPerMonth);
+                param[10] = new SqlParameter("@percentPerClosure", registerEntity.PercentPerClosure);
+                param[11] = new SqlParameter("@employeeSalary", registerEntity.EmployeeSalary);
+                param[12] = new SqlParameter("@address", registerEntity.Address);
+                param[13] = new SqlParameter("@presentCountry", registerEntity.PresentCountry);
+                param[14] = new SqlParameter("@presentState", registerEntity.PresentState);
+                param[15] = new SqlParameter("@presentCity", registerEntity.PresentCity);
+                param[16] = new SqlParameter("@presentArea", registerEntity.PresentArea);
+                param[17] = new SqlParameter("@pinCode", registerEntity.Pincode);
+                param[18] = new SqlParameter("@employmentStatus", registerEntity.EmploymentStatus);
+                param[19] = new SqlParameter("@jobType", registerEntity.JobType);
+                param[20] = new SqlParameter("@clientSite", registerEntity.ClientSite);
+                param[21] = new SqlParameter("@clientName", registerEntity.ClientName);
+                param[22] = new SqlParameter("@panCardNo", registerEntity.PANCardNo);
+                param[23] = new SqlParameter("@photoPath", registerEntity.PhotoPath);
+                param[24] = new SqlParameter("@password", registerEntity.Password);
+                param[25] = new SqlParameter("@isMailSent", registerEntity.IsMailSent);
+                param[26] = new SqlParameter("@setHrid", SqlDbType.VarChar, 500);
+                param[26].Direction = ParameterDirection.Output;
 
                 SqlHelper.ExecuteNonQuery(connection, CommandType.StoredProcedure, Constants.sp_AD_InsertHRRegisterDetails, param);
-                string recruiterID = Convert.ToString(param[25].Value);
+                string recruiterID = Convert.ToString(param[26].Value);
 
                 if (string.IsNullOrEmpty(recruiterID))
                 {
